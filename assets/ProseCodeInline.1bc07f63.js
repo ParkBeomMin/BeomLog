b45a33d93ea3ea1225d8b5578de3f8538d357ce0
bmpark <bmpark@jinhak.com>
2023-08-21 11:44:17 +0900

    Updates

--- assets/ProseCodeInline.1bc07f63.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as n,c as r,ak as c}from"./entry.da1be7d2.js";const s={};function t(e,a){return n(),r("code",null,[c(e.$slots,"default")])}const _=o(s,[["render",t]]);export{_ as default};
--- assets/ProseThead.1548c502.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,ak as a}from"./entry.da1be7d2.js";const s={};function c(e,n){return r(),t("thead",null,[a(e.$slots,"default")])}const _=o(s,[["render",c]]);export{_ as default};
