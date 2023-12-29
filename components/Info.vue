0018f35a44bf68ba89fc08e88aa07f9c17db437c
bmpark <club20608@gmail.com>
2023-12-29 14:06:28 +0900

    keyword 수정

--- components/Info.vue
+++ components/Info.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="flex flex-row w-full p-6 border-b-2 border-slate-300 mb-4">
-        <div class="hidden md:block basis-1/4">
-            <img class="m-auto w-24 h-24 rounded-full object-cover border-double border-2 border-slate-300 transition duration-500 hover:scale-125" src="~/assets/profile.jpg" alt="profile" />
-        </div>
-        <div class="mx-auto md:m-0 text-center md:text-left">
-            <h3 class="mb-1 font-bold text-lg">박범민</h3>
-            <span class="mr-2 p-1 md:rounded-md bg-[#fafafa] flex before:content-['✨'] md:inline" v-for="(keyword, i) of keywordList" :key="`keyword-${i}`">{{ keyword }}</span>
-            <SocialList class="justify-center md:justify-start" />
-        </div>
+  <div class="flex flex-row w-full p-6 border-b-2 border-slate-300 mb-4">
+    <div class="hidden md:block basis-1/4">
+      <img
+        class="m-auto w-24 h-24 rounded-full object-cover border-double border-2 border-slate-300 transition duration-500 hover:scale-125"
+        src="~/assets/profile.jpg"
+        alt="profile"
+      />
     </div>
+    <div class="mx-auto md:m-0 text-center md:text-left">
+      <h3 class="mb-1 font-bold text-lg">박범민</h3>
+      <span
+        class="mr-2 p-1 md:rounded-md bg-[#fafafa] flex before:content-['✨'] md:inline"
+        v-for="(keyword, i) of keywordList"
+        :key="`keyword-${i}`"
+        >{{ keyword }}</span
+      >
+      <SocialList class="justify-center md:justify-start" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-const keywordList = reactive(['FrontEnd Engineer', '까망베르 집사🐈🐈‍⬛', 'DX']);
+const keywordList = reactive(["Web Engineer", "까망베르 집사🐈🐈‍⬛", "DX"]);
 </script>
