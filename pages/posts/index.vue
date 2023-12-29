429f2e7050bc9c78e877699f272e74a3d256d864
bmpark <club20608@gmail.com>
2023-12-29 09:58:04 +0900

    검색 수정

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,42 +1,49 @@
 <template>
-    <h3 class="mt-16 font-bold text-2xl text-center">포스트를 검색해보세요.</h3>
-    <IconInput class="mt-8" id="search" type="search" :placeholder="randomPlaceholder" v-model="postState.keyword" @onClick="getPostList" />
-    <KakaoBannerLong />
-    <CategoryFilter class="mt-8" />
-    <PostList />
-    <KakaoBannerBox />
+  <h3 class="mt-16 font-bold text-2xl text-center">포스트를 검색해보세요.</h3>
+  <IconInput
+    class="mt-8"
+    id="search"
+    type="search"
+    :placeholder="randomPlaceholder"
+    v-model="postState.keyword"
+    @onClick="getPostList_"
+  />
+  <KakaoBannerLong />
+  <CategoryFilter class="mt-8" />
+  <PostList />
+  <KakaoBannerBox />
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-    documentDriven: {
-        page: false,
-        // Keep page fetching enabled surround: false // Disable surround fetching
-    },
+  documentDriven: {
+    page: false,
+    // Keep page fetching enabled surround: false // Disable surround fetching
+  },
 });
 useSeoMeta({
-    title: 'Beom Log | Posts',
-    ogTitle: 'Beom Log | Posts',
-    description: '프론트엔드 기술블로그 입니다.',
-    ogDescription: '프론트엔드 기술블로그 입니다.',
-    ogImage: '/banner.jpg',
+  title: "Beom Log | Posts",
+  ogTitle: "Beom Log | Posts",
+  description: "프론트엔드 기술블로그 입니다.",
+  ogDescription: "프론트엔드 기술블로그 입니다.",
+  ogImage: "/banner.jpg",
 });
 useHead({
-    title: 'Beom Log | Posts',
-    meta: [
-        {
-            name: 'keywords',
-            content: '기술블로그, JavaScript, Vue, Nuxt, FrontEnd, 프론트엔드',
-        },
-        { name: 'robots', content: 'index, follow' },
-    ],
+  title: "Beom Log | Posts",
+  meta: [
+    {
+      name: "keywords",
+      content: "기술블로그, JavaScript, Vue, Nuxt, FrontEnd, 프론트엔드",
+    },
+    { name: "robots", content: "index, follow" },
+  ],
 });
 
 const { postState, getPostList } = usePosts();
 
 const randomPlaceholder = computed(() => {
-    const random = Math.floor(Math.random() * postState.value.postList.length);
-    return postState.value.postList[random]?.title;
+  const random = Math.floor(Math.random() * postState.value.postList.length);
+  return postState.value.postList[random]?.title;
 });
 
 // onBeforeRouteLeave((to, from) => {
@@ -44,4 +51,10 @@ const randomPlaceholder = computed(() => {
 //         resetFilter();
 //     }
 // });
+
+const getPostList_ = async () => {
+  await useAsyncData("posts", async () => {
+    await getPostList();
+  });
+};
 </script>
